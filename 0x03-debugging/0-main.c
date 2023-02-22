defa0b83eeef664986812058bd1a965f2ee6d384
Ntombiyenkosi <ntombiyenkosi1995@gmail.com>
2023-02-22 09:36:38 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Test function for positive or negative numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
